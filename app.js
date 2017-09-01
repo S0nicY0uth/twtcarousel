ec424cc69d14212687cd11ab0455b3d2c4e791ba
Chris Hopkins <cjhopkins1986@gmail.com>
2017-09-01 18:57:36 +0100

    adding header info attempt 13

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ function gotData(err, data, response) {
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
