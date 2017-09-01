35f07d2f7de9b49f71b9de4fa42494f4c3d7ef56
Chris Hopkins <cjhopkins1986@gmail.com>
2017-09-01 12:53:47 +0100

    adding header info

--- app.js
+++ app.js
@@ -10,6 +10,27 @@ var T = new Twit({
 });
 var params = { screen_name: '@phenixtest', count: 100 };
 
+//header shit
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.bail.phenixcustomers.co.uk/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 T.get('statuses/user_timeline', params, gotData);
 
 
